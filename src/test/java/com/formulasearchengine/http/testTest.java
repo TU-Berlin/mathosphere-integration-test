98190bdc92b0e053fe62402cba64bfcc33b1c12c
Moritz Schubotz <wiki@physikerwelt.de>
2017-07-04 15:51:15 +0900

    Basic test against local mathoid instance

---
+++ src/test/java/com/formulasearchengine/http/testTest.java
@@ -0,0 +1,19 @@
+package com.formulasearchengine.http;
+
+import com.google.common.collect.Multiset;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Moritz on 04.07.2017.
+ */
+final public class testTest {
+    @Test
+    public void myTest() throws Exception {
+        final Multiset<String> identifiers = test.getIdentifiers("a+b+c", "http://localhost:10044/texvcinfo");
+        assertTrue(identifiers.contains("a"));
+        assertTrue(identifiers.contains("c"));
+        assertTrue(identifiers.contains("b"));
+    }
+}
